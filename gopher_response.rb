4d767104442a9710f3d22cf847296053228a769d
Tom Clark <tclark@requisitesystems.com>
2009-07-20 21:48:51 -0400

    added a response line object to aid processing of responses

--- gopher_response.rb
+++ gopher_response.rb
@@ -1,9 +1,31 @@
 class GopherResponse
   def initialize(r)
-    @full_response = r.split(/\r\n/)
+   @response_lines = r.split(/\r\n/).map { |l| GopherResponseLine.new(l) }
   end
   
   def print
-    @full_response.each {|line| puts line.chop + "\n"}
+    #@response_lines.each {|line| puts line.type + " " + line.port + "\n"}
+    @response_lines.each {|line| puts line.type + " " + line.label + " " + line.path + " " + line.host + " " + line.port }
+  end
+end
+
+class GopherResponseLine
+  attr_reader :text, :type, :port, :host, :path, :label
+  def initialize(line)
+    @text = line
+    match = line.match(/(\w)(.+?)\t(.+?)\t(\S+)\t(\d+)\t*\+{0,1}$/)
+    if match
+      @type = match[1]
+      @label = match[2]
+      @path = match[3]
+      @host = match[4]
+      @port = match[5]
+    else
+      @type = ''
+      @label = ''
+      @path = ''
+      @host = ''
+      @port = ''
+    end
   end
 end
